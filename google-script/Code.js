8bdf4982a4e2a8602a055407dbd57384bbd62d5e
Jakub Bouček <pan@jakubboucek.cz>
2020-09-24 02:33:27 +0200

    Modernize backend Script (to V8)

--- google-script/Code.js
+++ google-script/Code.js
@@ -1,18 +1,18 @@
-var VOTE_LIST_FIRST_NAME_ROW = 15;
+const VOTE_LIST_FIRST_NAME_ROW = 15;
 
 function clearOldVotes() {
-  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  const sheets = spreadsheet.getSheets();
-  
-  sheets.shift(); // List
-  sheets.shift(); // Hlas 1
-  
-  for(const sheet of sheets) {
-    spreadsheet.deleteSheet(sheet);
-  }
-  
-  spreadsheet.getSheetByName("Hlasování č. 1").activate();
-  clearSheetInludePersons();
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const sheets = spreadsheet.getSheets();
+
+    sheets.shift(); // List
+    sheets.shift(); // Hlas 1
+
+    for (const sheet of sheets) {
+        spreadsheet.deleteSheet(sheet);
+    }
+
+    spreadsheet.getSheetByName("Hlasování č. 1").activate();
+    clearSheetInludePersons();
 }
 
 function onOpen() {
@@ -28,7 +28,7 @@ function include(filename) {
 }
 
 function showSidebar() {
-    var html = HtmlService
+    const html = HtmlService
         .createTemplateFromFile('Panel')
         .evaluate()
         .setTitle('Hlasování')
@@ -38,7 +38,7 @@ function showSidebar() {
 }
 
 function getNamesSheet() {
-    var sheet = SpreadsheetApp.getActiveSheet();
+    const sheet = SpreadsheetApp.getActiveSheet();
     if (!sheet.getSheetName().match(/^Hlasování/)) {
         throw new Error("Toto není tabulka hlasování");
     }
@@ -47,30 +47,28 @@ function getNamesSheet() {
 }
 
 function getNamesRange() {
-    var sheet = getNamesSheet();
+    const sheet = getNamesSheet();
 
-    var last = sheet.getLastRow();
+    const last = sheet.getLastRow();
 
-    var range = sheet.getRange(VOTE_LIST_FIRST_NAME_ROW, 3, last - VOTE_LIST_FIRST_NAME_ROW + 1, 3);
-
-    return range;
+    return sheet.getRange(VOTE_LIST_FIRST_NAME_ROW, 3, last - VOTE_LIST_FIRST_NAME_ROW + 1, 3);
 }
 
 function getCurrentNames() {
-    var range = getNamesRange();
+    const range = getNamesRange();
 
-    var values = range.getValues();
+    const values = range.getValues();
 
     //Hack for unique merged sorded objects
-    var names = {};
-    var order = [];
-    for (y in values) {
-        var name = values[y][0];
-        var value = values[y][2];
+    const names = {};
+    const order = [];
+    for (const y of values) {
+        const name = y[0];
+        const value = y[2];
         if (name === '') continue;
 
-        var isNew = names[name] === undefined;
-        var isFilled = value !== '';
+        const isNew = names[name] === undefined;
+        const isFilled = value !== '';
 
         // Merge - set as filled when a least one item is filled
         names[name] = isFilled || (isNew ? false : names[name]);
@@ -79,9 +77,8 @@ function getCurrentNames() {
 
     order.sort();
 
-    var output = [];
-    for (i in order) {
-        var name = order[i];
+    const output = [];
+    for (const name of order) {
         output.push({
             'name': name,
             'filled': names[name]
@@ -92,61 +89,64 @@ function getCurrentNames() {
 }
 
 function vote(name, value) {
-    var range = getNamesRange();
-    var values = range.getValues();
-    for (y in values) {
-        if (values[y][0] == name) {
+    const range = getNamesRange();
+    const values = range.getValues();
+    for (const y in values) {
+        if (values.hasOwnProperty(y) === false) continue;
+        if (values[y][0] === name) {
             range.getCell(parseInt(y) + 1, 3).setValue(value);
         }
     }
 }
 
 function voteRest(value) {
-    var range = getNamesRange();
-    var values = range.getValues();
-    for (y in values) {
-        if (values[y][2] == '') {
+    const range = getNamesRange();
+    const values = range.getValues();
+    for (const y in values) {
+        if (values.hasOwnProperty(y) === false) continue;
+        if (values[y][2] === '') {
             range.getCell(parseInt(y) + 1, 3).setValue(value);
         }
     }
 }
 
 function clearAllVotes() {
-    var range = getNamesRange();
-    var values = range.getValues();
-    for (y in values) {
-        range.getCell(parseInt(y) + 1, 3).setValue(values[y][0] == '' ? 'Nepřítomen' : '');
+    const range = getNamesRange();
+    const values = range.getValues();
+    for (const y in values) {
+        if (values.hasOwnProperty(y) === false) continue;
+        range.getCell(parseInt(y) + 1, 3).setValue(values[y][0] === '' ? 'Nepřítomen' : '');
     }
 }
 
 function clearAll() {
-    sheet = getNamesSheet();
+    const sheet = getNamesSheet();
     clearAllVotes();
     sheet.getRange("A7").setValue('');
 }
 
 function clearSheetInludePersons() {
-    sheet = getNamesSheet();
+    const sheet = getNamesSheet();
     const namesRange = getNamesRange();
-    
+
     const values = namesRange.getValues();
-    for(const i in values) {
-      values[i][0] = '';
+    for (const value of values) {
+        value[0] = '';
     }
     namesRange.setValues(values);
-    
+
     clearAllVotes();
 }
 
 function createNewVoteSheet() {
-    var sheet = getNamesSheet(); //only test to valid active sheet
-    var name = sheet.getName();
+    let sheet = getNamesSheet(); //only test to valid active sheet
+    let name = sheet.getName();
 
-    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
     sheet = spreadsheet.duplicateActiveSheet();
 
-    var numMatch = name.match(/\d+/);
+    let numMatch = name.match(/\d+/);
     if (numMatch) {
         numMatch = parseInt(numMatch[0]) + 1;
         name = name.replace(/\d+/, numMatch);
